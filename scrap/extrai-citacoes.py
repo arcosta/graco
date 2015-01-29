7c570b5a416e8325aec4e68f5661b51f91a2415c
Aurelio Costa <arcosta@gmail.com>
2015-01-29 11:56:35 -0200

    Changed to use updated version of py2neo

--- scrap/extrai-citacoes.py
+++ scrap/extrai-citacoes.py
@@ -1,17 +1,23 @@
-from py2neo import neo4j,cypher,node
+#from py2neo import neo4j,cypher,node
+from py2neo import Graph
 import re
 
 
-graph_db = neo4j.GraphDatabaseService()
-queryArticles = "MATCH (a:Article) RETURN a.citacao"
-citations,metadata = cypher.execute(graph_db, queryArticles)
+graph_db = Graph()
+
+queryArticles = "MATCH (a:Article) RETURN a.authors"
+citations = graph_db.cypher.execute(queryArticles)
 citationPrefix= re.compile("(.*(\.)?[\d]{4}(\n)?)",re.IGNORECASE | re.MULTILINE)
 
 
 for cit in citations:
     citationAsList = cit[0].split(" . ")
-    prefixToRemove = citationPrefix.match(citationAsList[0])
-    citationAsString = citationPrefix.split(citationAsList[0])[4].lstrip()
-    print("Autores: %s" % citationAsString.split(" ; "))
-    print("Titulo: %s \n" % citationAsList[1])
+    #prefixToRemove = citationPrefix.match(citationAsList[0])
+    
+    #citationAsString = citationPrefix.split(citationAsList[0])[4].lstrip()
+    #authors = citationAsString.split(" ; ")
+    
+    authors = citationAsList[0].split(" ; ")
+    print("Autores(%i): %s" % (len(authors), [a.replace('\n','').replace('\t','') for a in authors]))
+    #print("Titulo: %s \n" % citationAsList[1])
             
