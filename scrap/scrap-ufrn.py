953ea60f44ccaba34f2f2a10f6f5d856daa62d30
Aurelio Costa <arcosta@gmail.com>
2015-11-24 12:35:37 -0200

    Fix unicode problem when parse ufrn page

--- scrap/scrap-ufrn.py
+++ scrap/scrap-ufrn.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import sys
@@ -7,12 +8,12 @@ import sys
  Universidade Federal do Rio Grande do Norte/Brazil
 '''
 
-baseurl = "https://sigaa.ufrn.br/sigaa/public/programa/equipe.jsf?lc=pt_BR&id=73"
+baseurl = u"https://sigaa.ufrn.br/sigaa/public/programa/equipe.jsf?lc=pt_BR&id=73"
 
 
 professors=list()
 
-soup =bs(urlopen(baseurl).read())
+soup =bs(urlopen(baseurl).read(), from_encoding='utf-8')
 
 table = soup.find(id="table_lt")
 if not table:
@@ -24,15 +25,17 @@ for tr in table.find_all('tr'):
     row = tr.find_all('td')
     a_tag = row[4].find('a')
     if a_tag:
-        print('%s: %s' %(row[0].text.strip(), a_tag['href']))
+        print(u'%s: %s' %(row[0].text.strip(), a_tag['href']))
         professors.append({"name":row[0].text.strip(), "lattes": a_tag['href']})
     else:
-        print('%s: %s' %(row[0].text.strip(), 'x'))
+        print(u'%s: %s' %(row[0].text.strip(), 'x'))
         professors.append({"name":row[0].text.strip(), "lattes": ""})
         
 with open("docentes-ufrn.json",'w') as f:
     f.write('[')
     for p in professors:
-        f.write(u'{"name":"%s","lattes":"%s"},' % (p['name'],p['lattes']))
+        if p['name'] == 'Nome':
+            continue
+        f.write('{"name":"%s","lattes":"%s"},' % (p['name'],p['lattes']))
     f.write('null]')
         
